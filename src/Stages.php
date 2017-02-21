a10c37aa9328843984eff600be4d62db346ee13e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-02-21 22:20:48 +0100

    Stages

---
+++ src/Stages.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Travis\LogParser;
+
+final class Stages
+{
+    const BEFORE_INSTALL = 'before_install';
+    const INSTALL        = 'install';
+    const BEFORE_SCRIPT  = 'before_script';
+    const SCRIPT         = 'script';
+    const BEFORE_CACHE   = 'before_cache';
+    const AFTER_SUCCESS  = 'after_success';
+    const AFTER_FAILURE  = 'after_failure';
+    const AFTER_SCRIPT   = 'after_script';
+}
