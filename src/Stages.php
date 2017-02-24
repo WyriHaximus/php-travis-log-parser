d499ab9ebfca5f5f9753e331fb31790823a3c404
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-02-24 17:48:10 +0100

    Added two stage for jobs that haven't started yet and jobs are done

--- src/Stages.php
+++ src/Stages.php
@@ -4,12 +4,14 @@ namespace WyriHaximus\Travis\LogParser;
 
 final class Stages
 {
-    const BEFORE_INSTALL = 'before_install';
-    const INSTALL        = 'install';
-    const BEFORE_SCRIPT  = 'before_script';
-    const SCRIPT         = 'script';
-    const BEFORE_CACHE   = 'before_cache';
-    const AFTER_SUCCESS  = 'after_success';
-    const AFTER_FAILURE  = 'after_failure';
-    const AFTER_SCRIPT   = 'after_script';
+    const NOT_STARTED_YET = 'not_started_yet';
+    const BEFORE_INSTALL  = 'before_install';
+    const INSTALL         = 'install';
+    const BEFORE_SCRIPT   = 'before_script';
+    const SCRIPT          = 'script';
+    const BEFORE_CACHE    = 'before_cache';
+    const AFTER_SUCCESS   = 'after_success';
+    const AFTER_FAILURE   = 'after_failure';
+    const AFTER_SCRIPT    = 'after_script';
+    const DONE            = 'done';
 }
