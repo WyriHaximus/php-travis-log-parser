46c966b616cbc663499ae77e5082ff2110aca553
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-02-25 22:40:09 +0100

    Parse commands from the config and emit State objects on the observable

--- src/Parser.php
+++ src/Parser.php
@@ -2,15 +2,28 @@
 
 namespace WyriHaximus\Travis\LogParser;
 
+use InvalidArgumentException;
 use Exception;
 use Rx\Observable;
 use Rx\ObserverInterface;
 use Rx\SchedulerInterface;
+use WyriHaximus\Travis\ConfigParser\Action;
 use WyriHaximus\Travis\ConfigParser\Config;
 
 final class Parser
 {
-    const LF = "\r\n";
+    const LF = "\r";
+    const ACTION_PREFIX = "\e[0K$ ";
+    const STAGES = [
+        Stages::BEFORE_INSTALL,
+        Stages::INSTALL,
+        Stages::BEFORE_SCRIPT,
+        Stages::SCRIPT,
+        Stages::BEFORE_CACHE,
+        Stages::AFTER_SUCCESS,
+        Stages::AFTER_FAILURE,
+        Stages::AFTER_SCRIPT,
+    ];
 
     /**
      * @var Observable
@@ -21,6 +34,11 @@ final class Parser
      * @var Config
      */
     private $config;
+
+    /**
+     * @var State
+     */
+    private $state;
 
     /**
      * @var string
@@ -41,6 +59,29 @@ final class Parser
     {
         $this->stream = $stream;
         $this->config = $config;
+        $this->state = new State();
+
+        $this->setUpState($config);
+    }
+
+    private function setUpState(Config $config)
+    {
+        foreach (self::STAGES as $stage) {
+            if (!isset($config->yaml[$stage])) {
+                continue;
+            }
+
+            if (is_string($config->yaml[$stage])) {
+                $this->state = $this->state->withStage($stage, new Action($config->yaml[$stage]));
+                continue;
+            }
+
+            $actions = [];
+            foreach ($config->yaml[$stage] as $action) {
+                $actions[] = new Action($action);
+            }
+            $this->state = $this->state->withStage($stage, ...$actions);
+        }
     }
 
     private function parse(): Observable
@@ -49,6 +90,7 @@ final class Parser
             ObserverInterface $observer,
             SchedulerInterface $scheduler
         ) {
+            $observer->onNext($this->state);
             $this->stream->subscribeCallback(
                 function (string $data) use ($observer) {
                     $this->buffer .= $data;
@@ -58,6 +100,8 @@ final class Parser
                     $observer->onError($error);
                 },
                 function () use ($observer) {
+                    $this->state = $this->state->withCurrentStage(Stages::DONE);
+                    $observer->onNext($this->state);
                     $observer->onCompleted();
                 },
                 $scheduler
@@ -72,26 +116,45 @@ final class Parser
         }
 
         $lines = $this->extractLines();
-
         foreach ($lines as $line) {
-            $observer->onNext($line);
+            if (strpos($line, self::ACTION_PREFIX) !== 0) {
+                continue;
+            }
+
+            $this->processLine($line, $observer);
         }
     }
 
     private function extractLines(): array
     {
+        $this->buffer = str_replace("\n", '', $this->buffer);
         $bufferLength = strlen($this->buffer);
         $lastLFPosition = strrpos($this->buffer, self::LF);
 
-        if ($lastLFPosition + 2 === $bufferLength && $lastLFPosition === strpos($this->buffer, self::LF)) {
+        if ($lastLFPosition + strlen(self::LF) === $bufferLength &&
+            $lastLFPosition === strpos($this->buffer, self::LF)
+        ) {
             $lines = [substr($this->buffer, 0, $lastLFPosition)];
             $this->buffer = '';
             return $lines;
         }
 
         $buffer = substr($this->buffer, 0, $lastLFPosition);
-        $this->buffer = substr($this->buffer, $lastLFPosition + 2);
+        $this->buffer = substr($this->buffer, $lastLFPosition + strlen(self::LF));
 
         return explode(self::LF, $buffer);
+    }
+
+    private function processLine(string $line, ObserverInterface $observer)
+    {
+        $line = ltrim($line, self::ACTION_PREFIX);
+        $action = new Action($line);
+        try {
+            $stage = $this->state->getStageByAction($action);
+            $this->state = $this->state->withCurrentStage($stage)->withCurrentAction($action);
+            $observer->onNext($this->state);
+        } catch (InvalidArgumentException $error) {
+            // Do nothing, this action isn't configured and could very well be once of travis' own actions
+        }
     }
 }
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -7,59 +7,153 @@ use Rx\Observable;
 use WyriHaximus\Travis\ConfigParser\Config;
 use WyriHaximus\Travis\LogParser\Parser;
 use Generator;
+use WyriHaximus\Travis\LogParser\Stages;
+use WyriHaximus\Travis\LogParser\StageState;
+use WyriHaximus\Travis\LogParser\State;
 
 final class ParserTest extends TestCase
 {
     public function linesProvider(): Generator
     {
+        $randomBytes = bin2hex(random_bytes(256));
+
+        $notYetStarted = function (State $state) {
+            self::assertSame(Stages::NOT_STARTED_YET, $state->getCurrentStage());
+        };
+        $done = function (State $state) {
+            self::assertSame(Stages::DONE, $state->getCurrentStage());
+        };
+
         yield [
             "abc\r\n",
-            ['abc'],
+            new Config([]),
+            [
+                $notYetStarted,
+                $done,
+            ],
         ];
 
         yield [
-            "abc\r\ndef\r\n",
-            ['abc', 'def'],
+            "abc\r\ndef\r\n" . Parser::ACTION_PREFIX . "make travis\r\n",
+            new Config([
+                'script' => 'make travis',
+            ]),
+            [
+                $notYetStarted,
+                function (State $state) {
+                    self::assertSame(Stages::SCRIPT, $state->getCurrentStage());
+                    self::assertSame('make travis', (string)$state->getCurrentAction());
+                    self::assertSame(1, $state->getCurrentStep());
+                },
+                $done,
+            ],
         ];
 
-        $randomBytes = bin2hex(random_bytes(256));
         yield [
-            "abc\r\ndef\r\n" . $randomBytes . "\r\n",
-            ['abc', 'def', $randomBytes],
+            "abc\r\ndef\r\n" . Parser::ACTION_PREFIX . "make travis\r\n" . $randomBytes . "\r\n",
+            new Config([
+                'script' => 'make travis',
+                'install' => [
+                    'composer install',
+                    'composer update',
+                ],
+            ]),
+            [
+                $notYetStarted,
+                function (State $state) {
+                    self::assertSame(Stages::SCRIPT, $state->getCurrentStage());
+                    self::assertSame('make travis', (string)$state->getCurrentAction());
+                    self::assertSame(1, $state->getCurrentStep());
+                },
+                $done,
+            ],
+        ];
+
+        yield [
+            "abc\r\ndef\r\n" .
+                Parser::ACTION_PREFIX . "make travis\r\n" .
+                $randomBytes . "\r\n" .
+                Parser::ACTION_PREFIX . "composer install\r\n" .
+                $randomBytes . "\r\n" .
+                Parser::ACTION_PREFIX . "composer update\r\n" .
+                $randomBytes . "\r\n" .
+                Parser::ACTION_PREFIX . "composer unknown\r\n" .
+                $randomBytes . "\r\n",
+            new Config([
+                'script' => 'make travis',
+                'install' => [
+                    'composer install',
+                    'composer update',
+                ],
+            ]),
+            [
+                $notYetStarted,
+                function (State $state) {
+                    self::assertSame(Stages::SCRIPT, $state->getCurrentStage());
+                    self::assertSame('make travis', (string)$state->getCurrentAction());
+                    self::assertSame(1, $state->getCurrentStep());
+                },
+                function (State $state) {
+                    self::assertSame(Stages::INSTALL, $state->getCurrentStage());
+                    self::assertSame('composer install', (string)$state->getCurrentAction());
+                    self::assertSame(1, $state->getCurrentStep());
+                },
+                function (State $state) {
+                    self::assertSame(Stages::INSTALL, $state->getCurrentStage());
+                    self::assertSame('composer update', (string)$state->getCurrentAction());
+                    self::assertSame(2, $state->getCurrentStep());
+                },
+                $done,
+            ],
         ];
     }
 
     /**
      * @dataProvider linesProvider
+     * @param string $contents
+     * @param Config $config
+     * @param callable[] $checks
      */
-    public function testCreateFromString(string $contents, array $lines)
+    public function testCreateFromString(string $contents, Config $config, array $checks)
     {
-        $array = [];
-
-        Parser::fromString(
-            $contents,
-            new Config([])
-        )->toArray()->subscribeCallback(function ($items) use (&$array) {
-            $array = $items;
-        });
-
-        self::assertSame($lines, $array);
+        $this->assertObservableResults(
+            Parser::fromString(
+                $contents,
+                $config
+            ),
+            $checks
+        );
     }
 
     /**
      * @dataProvider linesProvider
+     * @param string $contents
+     * @param Config $config
+     * @param callable[] $checks
      */
-    public function testCreateFromObservable(string $contents, array $lines)
+    public function testCreateFromObservable(string $contents, Config $config, array $checks)
+    {
+        $this->assertObservableResults(
+            Parser::fromObservable(
+                Observable::fromArray(str_split($contents)),
+                $config
+            ),
+            $checks
+        );
+    }
+
+    private function assertObservableResults(Observable $observable, array $checks)
     {
         $array = [];
 
-        Parser::fromObservable(
-            Observable::fromArray(str_split($contents)),
-            new Config([])
-        )->toArray()->subscribeCallback(function ($items) use (&$array) {
+        $observable->toArray()->subscribeCallback(function ($items) use (&$array) {
             $array = $items;
         });
 
-        self::assertSame($lines, $array);
+        foreach ($array as $item) {
+            /** @var callable $check */
+            $check = array_shift($checks);
+            $check($item);
+        }
     }
 }
